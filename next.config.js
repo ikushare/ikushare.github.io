e0ec80c95c84309dbf024441357487bd36e1c781
ikushare <79681251+ikushare@users.noreply.github.com>
2024-08-16 11:32:13 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const IsDevelopment = process.env.NODE_ENV === "development";
 const nextConfig = {
     pageExtensions: ["js", "jsx", "ts", "tsx"],
     //assetPrefix: "",
-    assetPrefix: IsDevelopment ? "" : "/ikushare",
+    assetPrefix: IsDevelopment ? "" : "/ikushare.github.io",
 };
 
 module.exports = withPlugins([], {
