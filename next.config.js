b2259301b92ff0cd653b2230f8b13427a3018537
ikushare <79681251+ikushare@users.noreply.github.com>
2024-08-16 11:05:13 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,8 +6,8 @@ const webpackconfig = require("./webpack.config");
 const IsDevelopment = process.env.NODE_ENV === "development";
 const nextConfig = {
     pageExtensions: ["js", "jsx", "ts", "tsx"],
-    // assetPrefix: "",
-    assetPrefix: IsDevelopment ? "" : "/castalia",
+    assetPrefix: "",
+    //assetPrefix: IsDevelopment ? "" : "/castalia",
 };
 
 module.exports = withPlugins([], {
