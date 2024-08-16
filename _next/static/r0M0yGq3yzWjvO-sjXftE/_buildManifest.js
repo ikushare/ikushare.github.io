104831be42324ba93e8014434f9282e10be256bd
ikushare <79681251+ikushare@users.noreply.github.com>
2024-08-16 06:57:24 +0000

    Deploying to gh-pages from @ ikushare/ikushare.github.io@672275129d97284320018e7daad618f0e98d608a 🚀

---
+++ _next/static/r0M0yGq3yzWjvO-sjXftE/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/503-be69965ed339c9efac53.js","static/chunks/pages/index-e1e26eae87cb1c96812f.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
